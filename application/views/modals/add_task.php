2af47a139eff1925bce7dccc37d53b1a951cd44b
megabreakage <martin.njau@live.com>
2019-01-26 23:30:44 +0300

    Adds required attribute to compulsary input fields

--- application/views/modals/add_task.php
+++ application/views/modals/add_task.php
@@ -20,7 +20,7 @@
                   <div class="row">
                     <div class="col-xl-6">
                       <label>Aircraft Reg</label>
-                      <select class="form-control" name="aircraft_id">
+                      <select class="form-control" name="aircraft_id" required>
                         <option value=""> -- select aircraft --</option>
                         <?php foreach ($aircrafts as $aircraft): ?>
                           <option value="<?php echo $aircraft['aircraft_id']; ?>"><?php echo $aircraft['aircraft_reg']; ?></option>
@@ -41,7 +41,7 @@
                   <div class="row">
                     <div class="col-xl-4">
                       <label>Category</label>
-                      <select class="form-control" name="comp_cat_id">
+                      <select class="form-control" name="comp_cat_id" required>
                         <option value=""> -- select category --</option>
                         <?php foreach ($comp_cats as $comp_cat): ?>
                           <option value="<?php echo $comp_cat['comp_cat_id']; ?>"><?php echo $comp_cat['comp_cat']; ?></option>
@@ -62,7 +62,7 @@
                   <div class="row">
                     <div class="col-xl-3">
                       <label>Schedule type</label>
-                      <select class="form-control" name="schedule_type_id">
+                      <select class="form-control" name="schedule_type_id" required>
                         <option value=""> -- select --</option>
                         <?php foreach ($schedule_types as $schedule_type): ?>
                           <option value="<?php echo $schedule_type['type_id']; ?>"><?php echo $schedule_type['schedule_type']; ?></option>
@@ -71,7 +71,7 @@
                     </div>
                     <div class="col-xl-3">
                       <label>Task category</label>
-                      <select class="form-control" name="task_category_id">
+                      <select class="form-control" name="task_category_id" required>
                         <option value=""> -- select --</option>
                         <?php foreach ($task_categories as $task_cat): ?>
                           <option value="<?php echo $task_cat['task_category_id']; ?>"><?php echo $task_cat['task_category']; ?></option>
@@ -80,7 +80,7 @@
                     </div>
                     <div class="col-xl-3">
                       <label>Schedule category</label>
-                      <select class="form-control" name="schedule_cat_id">
+                      <select class="form-control" name="schedule_cat_id" required>
                         <option value=""> -- select --</option>
                         <?php foreach ($schedule_categories as $schedule_cat): ?>
                           <option value="<?php echo $schedule_cat['schedule_cat_id']; ?>"><?php echo $schedule_cat['schedule_category']; ?></option>
@@ -89,7 +89,7 @@
                     </div>
                     <div class="col-xl-3">
                       <label>Inspection type</label>
-                      <select class="form-control" name="inspection_id">
+                      <select class="form-control" name="inspection_id" required>
                         <option value=""> -- select --</option>
                         <?php foreach ($inspection_types as $inspection_type): ?>
                           <option value="<?php echo $inspection_type['inspection_id']; ?>"><?php echo $inspection_type['inspection']; ?></option>
@@ -100,7 +100,7 @@
                 </div>
                 <div class="col-xl-12 pb">
                   <label>ATA Chapter</label>
-                  <select class="form-control" name="ata_chapter_id">
+                  <select class="form-control" name="ata_chapter_id" required>
                     <option value=""> -- select ATA Chapter --</option>
                     <?php foreach ($ata_chapters as $ata_chapter): ?>
                       <option value="<?php echo $ata_chapter['ata_chapter_id']; ?>">00<?php echo $ata_chapter['ata_chapter']; ?>: <?php echo $ata_chapter['ata_name']; ?></option>
@@ -210,10 +210,10 @@
                           <div class="input-group-prepend">
                             <span class="input-group-text">Alarm:</span>
                           </div>
-                          <input id="alarm_cycles" class="form-control" type="text" name="alarm_cycles" placeholder="cycles" value="0" title="cycles">
-                          <input id="alarm_hours" class="form-control" type="text" name="alarm_hours" placeholder="hours" value="0" title="hours">
-                          <input id="alarm_calendar" class="form-control" type="text" name="alarm_calendar" placeholder=" Calendar" value="0">
-                          <select id="alarm_period" class="form-control" name="alarm_period">
+                          <input id="alarm_cycles" class="form-control" type="text" name="alarm_cycles" placeholder="cycles" value="0" title="cycles" required>
+                          <input id="alarm_hours" class="form-control" type="text" name="alarm_hours" placeholder="hours" value="0" title="hours" required>
+                          <input id="alarm_calendar" class="form-control" type="text" name="alarm_calendar" placeholder=" Calendar" value="0" required>
+                          <select id="alarm_period" class="form-control" name="alarm_period" required>
                             <option value="D">D</option>
                             <option value="M">M</option>
                             <option value="Y">Y</option>
@@ -230,25 +230,25 @@
                           <div class="input-group-prepend">
                             <span class="input-group-text">Last done:</span>
                           </div>
-                          <input id="last_done_cycles"  class="form-control" type="text" name="last_done_cycles" placeholder="cycles" value="0" title="cycles">
-                          <input id="last_done_hours"  class="form-control" type="text" name="last_done_hours" placeholder="hours" value="0" title="hours">
-                          <input id="last_done_date"  class="form-control" type="date" name="last_done_date" placeholder="Date" date>
+                          <input id="last_done_cycles"  class="form-control" type="text" name="last_done_cycles" placeholder="cycles" value="0" title="cycles" required>
+                          <input id="last_done_hours"  class="form-control" type="text" name="last_done_hours" placeholder="hours" value="0" title="hours" required>
+                          <input id="last_done_date"  class="form-control" type="date" name="last_done_date" placeholder="Date" title="date">
                         </div>
                         <div class="col-xl-12 input-group mb-3">
                           <div class="input-group-prepend">
                             <span class="input-group-text">Cumulative:</span>
                           </div>
-                          <input id="cum_cycles" class="form-control" type="text" name="cum_cycles" placeholder="cycles" value="0" title="cycles">
-                          <input id="cum_hours" class="form-control" type="text" name="cum_hours" placeholder="hours" value="0" title="hours">
+                          <input id="cum_cycles" class="form-control" type="text" name="cum_cycles" placeholder="cycles" value="0" title="cycles" required>
+                          <input id="cum_hours" class="form-control" type="text" name="cum_hours" placeholder="hours" value="0" title="hours" required>
                         </div>
                         <div class="col-xl-12 input-group mb-3">
                           <div class="input-group-prepend">
                             <span class="input-group-text">Next due:</span>
                           </div>
                           <!-- populate details from JavaScript/Db -->
-                          <input id="next_due_cycles" class="form-control" type="text" name="next_due_cycles" placeholder="cycles" value="" title="cycles" readonly>
-                          <input id="next_due_hours" class="form-control" type="text" name="next_due_hours" placeholder="hours" value="" readonly title="hours">
-                          <input id="next_due_date" class="form-control" type="text" name="next_due_date" placeholder="Date" value="" readonly date>
+                          <input id="next_due_cycles" class="form-control" type="text" name="next_due_cycles" placeholder="cycles" value="" readonly title="Due cycles">
+                          <input id="next_due_hours" class="form-control" type="text" name="next_due_hours" placeholder="hours" value="" readonly title="Due hours">
+                          <input id="next_due_date" class="form-control" type="text" name="next_due_date" placeholder="Date" value="" readonly title="Due date">
                         </div>
                         <div class="col-xl-12 text-right">
                           <!-- <button id="calcDues" class="aviaBtn btn btn-primary" type="button" >Calculate</button> -->
