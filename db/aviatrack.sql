c1931e8abbb61de47038d8f46cd446aef98a855f
megabreakage <martin.njau@live.com>
2019-01-27 14:33:25 +0300

    Updated db files

--- db/aviatrack.sql
+++ db/aviatrack.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 25, 2019 at 09:15 PM
--- Server version: 5.7.24-0ubuntu0.18.04.1
+-- Generation Time: Jan 27, 2019 at 01:29 PM
+-- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.1.17-0ubuntu0.17.10.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -53,7 +53,8 @@ INSERT INTO `aircrafts` (`aircraft_id`, `aircraft_reg`, `series`, `serial_number
 (3, '5Y-CGF', '200', '7373', 1, '2019-01-01', 4, 2, 1, 0, 32500.03, 27600, '2018-09-25', 1),
 (4, '5Y-CGH', '315', '525', 3, '2019-01-01', 2, 2, 1, 2, 28050.45, 23005, '2019-03-28', 1),
 (18, '5Y-CGL', '200', '523', 3, '2019-01-01', 1, 2, 1, 0, 72700.69, 39000, '2019-01-01', 1),
-(20, '5Y-NON', 'C208', '0036', 2, '1985-01-01', 5, 2, 2, 0, 22464.23, 30929, '2019-01-01', 1);
+(20, '5Y-NON', 'C208', '0036', 2, '1985-01-01', 5, 2, 2, 0, 22464.23, 30929, '2019-01-01', 1),
+(21, '5Y-BUZ', '100', '723', 3, '2019-01-01', 1, 2, 2, 0, 56500.55, 55495, '2019-01-01', 1);
 
 -- --------------------------------------------------------
 
@@ -114,7 +115,8 @@ INSERT INTO `ata_chapters` (`ata_chapter_id`, `ata_chapter`, `ata_name`, `catego
 (12, '11', 'Placards and Markings', 'Aircraft General'),
 (13, '12', 'Servicing', 'Aircraft General'),
 (14, '14', 'Hardware and General Tools', 'Aircraft General'),
-(15, '18', 'Variations and Nose Analysis(Helicopter only)', 'Aircraft General');
+(15, '18', 'Variations and Nose Analysis(Helicopter only)', 'Aircraft General'),
+(16, '32', 'Landing Gear', 'Airframe Systems');
 
 -- --------------------------------------------------------
 
@@ -163,7 +165,9 @@ CREATE TABLE `engines` (
 INSERT INTO `engines` (`engine_id`, `aircraft_id`, `number`, `model`, `serial_number`, `engine_cycles`, `engine_hours`, `update_date`, `posted_by`) VALUES
 (18, 18, 1, 'CF34', '6789098', 28750, 29800, '2019-01-18 12:19:54', 1),
 (19, 18, 2, 'CF34B', '6789098', 28750, 29800, '2019-01-18 12:19:54', 1),
-(22, 20, 1, 'PT6A-114A', 'PCE PC0657', 143364, 12245.2, '2019-01-18 13:03:39', 1);
+(22, 20, 1, 'PT6A-114A', 'PCE PC0657', 143364, 12245.2, '2019-01-18 13:03:39', 1),
+(23, 21, 1, 'PW121', '1209788', 38120, 38650.7, '2019-01-26 00:57:08', 1),
+(24, 21, 2, 'PW121', '170345', 35440, 35560.7, '2019-01-26 00:57:08', 1);
 
 -- --------------------------------------------------------
 
@@ -298,7 +302,7 @@ INSERT INTO `inspection_types` (`inspection_id`, `inspection`) VALUES
 (2, 'Discard'),
 (3, 'Functional Check'),
 (4, 'DVI'),
-(5, 'Inspect'),
+(5, 'Inspection'),
 (6, 'Life Limit'),
 (7, 'Major Inspection'),
 (8, 'Mechanical'),
@@ -386,9 +390,9 @@ CREATE TABLE `maintenance_type` (
 --
 
 INSERT INTO `maintenance_type` (`maint_type_id`, `maint_type`) VALUES
-(1, 'Discard'),
-(2, 'Initial'),
-(3, 'Repeat Interval'),
+(1, 'Initial'),
+(2, 'Repeat'),
+(3, 'Discard'),
 (4, 'Threshold');
 
 -- --------------------------------------------------------
@@ -437,7 +441,9 @@ CREATE TABLE `propellers` (
 INSERT INTO `propellers` (`propeller_id`, `aircraft_id`, `number`, `model`, `serial_number`, `propeller_cycles`, `propeller_hours`, `posted_by`) VALUES
 (5, 18, 1, 'DF567677', '98799', 1020, 1120, 1),
 (6, 18, 2, 'DF567677', '6578', 1020, 1120, 1),
-(9, 20, 1, 'McCauley', '993223', 9, 15789.5, 1);
+(9, 20, 1, 'McCauley', '993223', 9, 15789.5, 1),
+(10, 21, 1, 'McCauley', '780419', 0, 1750.45, 1),
+(11, 21, 2, 'McCauley', '678334', 0, 1679.72, 1);
 
 -- --------------------------------------------------------
 
@@ -448,32 +454,85 @@ INSERT INTO `propellers` (`propeller_id`, `aircraft_id`, `number`, `model`, `ser
 CREATE TABLE `schedules` (
   `schedule_id` int(11) NOT NULL,
   `aircraft_id` int(11) NOT NULL,
-  `task_card` varchar(25) NOT NULL,
+  `task_card` varchar(25) DEFAULT NULL,
   `task` text NOT NULL,
-  `description` text NOT NULL,
-  `part_number` varchar(25) NOT NULL,
-  `serial_number` varchar(25) NOT NULL,
+  `description` text,
+  `part_name` varchar(25) DEFAULT NULL,
+  `part_number` varchar(25) DEFAULT NULL,
+  `serial_number` varchar(25) DEFAULT NULL,
   `schedule_type_id` int(11) NOT NULL,
   `task_category_id` int(11) NOT NULL,
   `schedule_cat_id` int(11) NOT NULL,
   `comp_cat_id` int(11) NOT NULL,
   `inspection_id` int(11) NOT NULL,
   `ata_chapter_id` int(11) NOT NULL,
-  `zone` varchar(25) NOT NULL,
-  `location` varchar(25) NOT NULL,
-  `reference` text NOT NULL,
+  `zone` varchar(25) DEFAULT NULL,
+  `location` varchar(25) DEFAULT NULL,
+  `reference` text,
   `cum_cycles` int(11) NOT NULL,
-  `notes` text NOT NULL,
   `cum_hours` double NOT NULL,
-  `date_checked` date NOT NULL,
-  `next_due_date` date NOT NULL,
+  `last_done_cycles` int(11) NOT NULL,
+  `last_done_hours` double NOT NULL,
+  `date_checked` datetime NOT NULL,
+  `next_due_cycles` int(11) NOT NULL,
+  `next_due_hours` double NOT NULL,
+  `next_due_date` datetime NOT NULL,
+  `life_limit_cycles` int(11) NOT NULL,
+  `life_limit_hours` double NOT NULL,
+  `life_limit_calendar` int(11) NOT NULL,
+  `life_limit_period` varchar(1) NOT NULL,
   `alarm_cycles` int(11) NOT NULL,
   `alarm_hours` double NOT NULL,
   `alarm_calendar` int(11) NOT NULL,
-  `period` varchar(2) NOT NULL,
+  `alarm_period` varchar(1) NOT NULL,
   `date_posted` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `notes` text,
   `posted_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`schedule_id`, `aircraft_id`, `task_card`, `task`, `description`, `part_name`, `part_number`, `serial_number`, `schedule_type_id`, `task_category_id`, `schedule_cat_id`, `comp_cat_id`, `inspection_id`, `ata_chapter_id`, `zone`, `location`, `reference`, `cum_cycles`, `cum_hours`, `last_done_cycles`, `last_done_hours`, `date_checked`, `next_due_cycles`, `next_due_hours`, `next_due_date`, `life_limit_cycles`, `life_limit_hours`, `life_limit_calendar`, `life_limit_period`, `alarm_cycles`, `alarm_hours`, `alarm_calendar`, `alarm_period`, `date_posted`, `notes`, `posted_by`) VALUES
+(4, 21, '', '1A CHECK', '', '', '', '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 500, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 01:56:57', '', 1),
+(5, 21, '', '2A CHECK', '', '', '', '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 1000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 01:57:15', '', 1),
+(6, 21, '', '3A CHECK', '', '', '', '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 1500, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 02:11:27', '', 1),
+(7, 21, '', '4A CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 2000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 02:40:54', NULL, 1),
+(8, 21, '', '5A CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 2500, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 03:00:00', NULL, 1),
+(9, 21, '', '6A CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 3000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 05:37:57', NULL, 1),
+(10, 21, '', '9A CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 4500, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 05:52:04', NULL, 1),
+(11, 21, '', '1C CHECK', '', NULL, NULL, NULL, 3, 5, 7, 1, 5, 6, NULL, NULL, NULL, 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 5000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 19:59:07', NULL, 1),
+(12, 21, '', '2C CHECK', '', NULL, NULL, NULL, 3, 5, 7, 1, 5, 6, NULL, NULL, NULL, 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 10000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 19:59:18', NULL, 1),
+(14, 21, '', '3C CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 15000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 20:18:03', NULL, 1),
+(17, 21, '', '4C CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 20000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 20:28:40', NULL, 1),
+(18, 21, '', '5C CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 25000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 20:36:30', NULL, 1),
+(19, 21, '', '6C CHECK', NULL, '', NULL, '', 3, 5, 7, 1, 5, 6, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 0, 30000, '2019-03-31 00:00:00', 0, 0, 0, 'M', 0, 100, 2, 'M', '2019-01-26 20:42:46', NULL, 1),
+(20, 21, '', '', NULL, 'SHOCK STRUT', NULL, '08MDT0758', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-26 21:28:45', NULL, 1),
+(21, 21, '', '', NULL, 'DRAG STRUT', NULL, 'DCL451/95', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 01:41:14', NULL, 1),
+(22, 21, '', '', NULL, 'TRAILING ARM', NULL, 'DCL-433', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 03:11:45', NULL, 1),
+(23, 21, '', '', NULL, 'OUTTER CYLINDER', NULL, 'O7ALT0010', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 03:17:52', NULL, 1),
+(24, 21, '', '', NULL, 'INNER CYLINDER', NULL, 'SN07WHM', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 03:20:28', NULL, 1),
+(25, 21, '', '', NULL, 'PISTON SCHOCK STRUT', NULL, 'S10174', 2, 2, 1, 1, 11, 16, '', 'NLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 03:24:24', NULL, 1),
+(26, 21, '', '', NULL, 'AXLE', NULL, '07MBM1349', 2, 2, 1, 1, 11, 16, '', '', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 25000, 0, '2029-01-01 00:00:00', 60000, 0, 10, 'Y', 1000, 0, 6, 'M', '2019-01-27 03:29:58', NULL, 1),
+(27, 21, '', '', NULL, 'MLG SHOCK STRUT', NULL, '11MDT0781', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:22:12', NULL, 1),
+(28, 21, '', '', NULL, 'MLG DRAG STRUT', NULL, 'DCL685/96', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:25:59', NULL, 1),
+(29, 21, '', '', NULL, 'MLG YOKE ASSY', NULL, 'DCL639/95', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:28:19', NULL, 1),
+(30, 21, '', '', NULL, 'MLG STABILIZER', NULL, 'DCL705/95', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:31:00', NULL, 1),
+(31, 21, '', '', NULL, 'MLG PISTON', NULL, '10WHM160', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:34:17', NULL, 1),
+(32, 21, '', '', NULL, 'MLG CYLINDER', NULL, '09EXC1578', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:36:45', NULL, 1),
+(33, 21, '', '', NULL, 'MLG UPPER TQ LINK', NULL, '09EXC1744', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:40:19', NULL, 1),
+(34, 21, '', '', NULL, 'MLG LOWER TQ LINK', NULL, '10EXC1578', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:42:27', NULL, 1),
+(35, 21, '', '', NULL, 'MLG PIN STRUT TO YOKE', NULL, '11EXC5938', 2, 2, 1, 1, 11, 16, '', 'LH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 05:45:18', NULL, 1),
+(36, 21, '', '', NULL, 'SHOCK STRUT', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:12:24', NULL, 1),
+(37, 21, '', '', NULL, 'MLG DRAG STRUT', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:13:52', NULL, 1),
+(38, 21, '', '', NULL, 'MLG YOKE ASSY', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:15:06', NULL, 1),
+(39, 21, '', '', NULL, 'MLG STABILIZER STAY', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:15:58', NULL, 1),
+(40, 21, '', '', NULL, 'MLG PISTON', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:17:01', NULL, 1),
+(41, 21, '', '', NULL, 'MLG CYLINDER', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:18:47', NULL, 1),
+(42, 21, '', '', NULL, 'MLG UPPER TQ LINK', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:19:32', NULL, 1),
+(43, 21, '', '', NULL, 'MLG LOWER TQ LINK', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:20:49', NULL, 1),
+(44, 21, '', '', NULL, 'MLG PIN STRUT TO YOKE', NULL, '', 2, 2, 1, 1, 11, 16, '', 'RH MLG', '', 0, 0, 0, 0, '2019-01-01 00:00:00', 30000, 0, '2031-01-08 00:00:00', 60000, 0, 12, 'Y', 1000, 0, 6, 'M', '2019-01-27 06:22:06', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -516,6 +575,50 @@ CREATE TABLE `schedule_details` (
   `calendar` int(11) NOT NULL,
   `period` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schedule_details`
+--
+
+INSERT INTO `schedule_details` (`schedule_details_id`, `schedule_id`, `maint_type_id`, `cycles`, `hours`, `calendar`, `period`) VALUES
+(1, 4, 2, 0, 500, 3, 'M'),
+(2, 5, 2, 0, 1000, 6, 'M'),
+(3, 6, 2, 0, 1500, 9, 'M'),
+(4, 7, 2, 0, 2000, 12, 'M'),
+(5, 8, 2, 0, 2500, 15, 'M'),
+(6, 9, 2, 0, 3000, 18, 'M'),
+(7, 10, 1, 0, 4500, 21, 'M'),
+(10, 11, 2, 0, 5000, 24, 'M'),
+(11, 12, 2, 0, 10000, 36, 'M'),
+(12, 14, 2, 0, 15000, 48, 'M'),
+(13, 17, 2, 0, 20000, 60, 'M'),
+(14, 18, 2, 0, 25000, 72, 'M'),
+(15, 19, 2, 0, 30000, 84, 'M'),
+(16, 20, 2, 25000, 0, 10, 'Y'),
+(18, 21, 2, 25000, 0, 10, 'Y'),
+(20, 22, 2, 25000, 0, 10, 'Y'),
+(21, 23, 2, 25000, 0, 10, 'Y'),
+(22, 24, 2, 25000, 0, 10, 'Y'),
+(23, 25, 2, 25000, 0, 10, 'Y'),
+(24, 26, 2, 25000, 0, 10, 'Y'),
+(25, 27, 2, 30000, 0, 12, 'Y'),
+(26, 28, 2, 30000, 0, 12, 'Y'),
+(27, 29, 2, 30000, 0, 12, 'Y'),
+(28, 30, 2, 30000, 0, 12, 'Y'),
+(29, 31, 2, 30000, 0, 12, 'Y'),
+(30, 32, 2, 30000, 0, 12, 'Y'),
+(31, 33, 2, 30000, 0, 12, 'Y'),
+(32, 34, 2, 30000, 0, 12, 'Y'),
+(33, 35, 2, 30000, 0, 12, 'Y'),
+(34, 36, 2, 30000, 0, 12, 'Y'),
+(35, 37, 2, 30000, 0, 12, 'Y'),
+(36, 38, 2, 30000, 0, 12, 'Y'),
+(37, 39, 2, 30000, 0, 12, 'Y'),
+(38, 40, 2, 30000, 0, 12, 'Y'),
+(39, 41, 2, 30000, 0, 12, 'Y'),
+(40, 42, 2, 30000, 0, 12, 'Y'),
+(41, 43, 2, 30000, 0, 12, 'Y'),
+(42, 44, 2, 30000, 0, 12, 'Y');
 
 -- --------------------------------------------------------
 
@@ -761,7 +864,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `aircrafts`
 --
 ALTER TABLE `aircrafts`
-  MODIFY `aircraft_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `aircraft_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `aircraft_models`
 --
@@ -771,7 +874,7 @@ ALTER TABLE `aircraft_models`
 -- AUTO_INCREMENT for table `ata_chapters`
 --
 ALTER TABLE `ata_chapters`
-  MODIFY `ata_chapter_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ata_chapter_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `comp_cat`
 --
@@ -781,7 +884,7 @@ ALTER TABLE `comp_cat`
 -- AUTO_INCREMENT for table `engines`
 --
 ALTER TABLE `engines`
-  MODIFY `engine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `engine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `engineTypes`
 --
@@ -826,12 +929,12 @@ ALTER TABLE `manufacturers`
 -- AUTO_INCREMENT for table `propellers`
 --
 ALTER TABLE `propellers`
-  MODIFY `propeller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `propeller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 --
 -- AUTO_INCREMENT for table `schedule_category`
 --
@@ -841,7 +944,7 @@ ALTER TABLE `schedule_category`
 -- AUTO_INCREMENT for table `schedule_details`
 --
 ALTER TABLE `schedule_details`
-  MODIFY `schedule_details_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `schedule_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 --
 -- AUTO_INCREMENT for table `schedule_types`
 --
