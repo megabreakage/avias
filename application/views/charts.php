bf526d3a93cb9a75e2e1f22217830131bb323ee6
megabreakage <martin.njau@live.com>
2019-02-08 11:14:20 +0300

    Creates charts canvas display for pie, line, radar and bar graphs

--- application/views/charts.php
+++ application/views/charts.php
@@ -1,1 +1,24 @@
-<h2>Charts</h2>
+<!-- <h5>Charts</h5> -->
+
+<div class="row">
+  <div class="col-md-6 pb pt">
+    <canvas id="barChart" width="300" height="150" aria-label="Hello ARIA World" role="img">
+      <p>Bar Chart</p>
+    </canvas>
+  </div>
+  <div class="col-md-6 pb pt">
+    <canvas id="pieChart" width="300" height="150" aria-label="Hello ARIA World" role="img">
+      <p>Pie Chart</p>
+    </canvas>
+  </div>
+  <div class="col-md-6 pb pt">
+    <canvas id="lineChart" width="300" height="150" aria-label="Hello ARIA World" role="img">
+      <p>Line Chart</p>
+    </canvas>
+  </div>
+  <div class="col-md-6 pb pt">
+    <canvas id="radarChart" width="300" height="150" aria-label="Hello ARIA World" role="img">
+      <p>Radar Chart</p>
+    </canvas>
+  </div>
+</div>
