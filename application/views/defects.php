f2292352a098d74e812f84a7b5f2c264911cb830
megabreakage <martin.njau@live.com>
2019-02-09 08:52:14 +0300

    Creates search modules for defects

--- application/views/defects.php
+++ application/views/defects.php
@@ -1,15 +1,46 @@
 <h5>Defects</h5>
 <div class="row">
-  <div class="col-md-3 input-group">
+  <div class="col-md-2 input-group">
     <div class="input-group-prepend">
       <span class="input-group-text">Select Aircraft: </span>
     </div>
-    <select id="dfr_aircraft_reg" class="form-control" name="aircraft_reg">
+    <select id="d_search_by" class="form-control" name="aircraft_reg">
       <option value="all">All</option>
+      <option value="1">Aircraft Reg</option>
+      <option value="2">ATA Chapter</option>
+      <option value="3">Deferred</option>
+      <option value="4">Defered date Between</option>
+    </select>
+  </div>
+  <div id="d_aircraft_id" class="col-md-2 hidden">
+    <select id="dfr_aircraft_id" class="form-control">
       <?php foreach ($aircrafts as $aircraft): ?>
         <option value="<?php echo $aircraft['aircraft_id'] ?>"><?php echo $aircraft['aircraft_reg'] ?></option>
       <?php endforeach; ?>
     </select>
+  </div>
+  <div id="d_ata_id" class="col-md-2 hidden">
+    <select id="dfr_ata_id" class="form-control">
+      <?php foreach ($ata_chapters as $ata_chapter): ?>
+        <option value="<?php echo $ata_chapter['ata_chapter_id']; ?>"><?php echo $ata_chapter['ata_chapter']; ?>00: <?php echo $ata_chapter['ata_name']; ?></option>
+      <?php endforeach; ?>
+    </select>
+  </div>
+  <div id="d_status" class="col-md-2 hidden">
+    <select id="dfr_status" class="form-control">
+      <option value="Yes">Yes</option>
+      <option value="No">No</option>
+    </select>
+  </div>
+  <div id="d_dates" class="col-md-4 input-group mb3 hidden">
+    <div class="input-group-prepend">
+      <span class="input-group-text"> between </span>
+    </div>
+    <input id="dfr_from_date" type="date" class="form-control">
+    <div class="input-group-append input-group-prepend">
+      <span class="input-group-text"> and </span>
+    </div>
+    <input id="dfr_to_date" type="date" class="form-control">
   </div>
   <div class="col-md-12 pt pb models">
     <table class="table table-sm table-hover table-bodered table-striped">
@@ -26,7 +57,7 @@
           <th class="text-center">Expiry date</th>
         </tr>
       </thead>
-      <tbody id="flightDisplay">
+      <tbody id="defectsDisplay">
         <?php $i = 1; foreach ($defects as $defect): ?>
           <tr>
             <td><?php echo $i; ?>.</td>
