06f3970f8254ceffb007fa979a064b29394b5d3a
megabreakage <martin.njau@live.com>
2019-02-18 11:33:30 +0300

    Tweaks the appearance of the logout modal and adds a js file to the project

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -12,19 +12,19 @@
   <i class="fa fa-angle-up"></i>
 </a>
 <!-- Logout Modal-->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="logOut" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+        <h6 class="modal-title" id="exampleModalLabel">Ready to Leave?</h6>
         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
       </div>
       <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
       <div class="modal-footer">
-        <button class="aviaBtn btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a class="aviaBtn btn btn-primary" href="login.html">Logout</a>
+        <button class="aviaBtn btn" type="button" data-dismiss="modal">Cancel</button>
+        <a class="aviaBtn btn btn-danger" href="<?php echo base_url('auth/logout'); ?>">Logout</a>
       </div>
     </div>
   </div>
@@ -45,8 +45,7 @@
 <script src="<?php echo base_url(); ?>js/js.js"></script>
 <script src="<?php echo base_url(); ?>js/jsCharts.js"></script>
 <script src="<?php echo base_url(); ?>js/add_task.js"></script>
-
+<script src="<?php echo base_url(); ?>js/updates.js"></script>
 </div>
 </body>
-
 </html>
