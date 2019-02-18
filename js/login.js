a13ef2382893089619d456379ef4aae4e8a2db8d
megabreakage <martin.njau@live.com>
2019-02-18 11:41:49 +0300

    Creates js for logging in

---
+++ js/login.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("#login").submit(function(e){
+    e.preventDefault();
+    credentials = $(this).serialize();
+    $.ajax({
+      url: 'login_user',
+      method: 'POST',
+      dataType: 'json',
+      data: credentials,
+      success: function(data){
+        if (data === false | data === 0) {
+          $("#login_response").fadeIn(1000);
+          $("#login_response").removeClass('hidden');
+          $("#login_response").addClass('alert-danger');
+          $("#login_response").fadeOut(5000);
+        } else {
+          window.location.href = "http://192.168.2.114/avia";
+        }
+      }
+    });
+  });
+});
