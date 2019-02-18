232187f90b35dad7823ec570515c3cc27b76d031
megabreakage <martin.njau@live.com>
2019-02-18 11:27:06 +0300

    Adds function to open messages view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -37,6 +37,34 @@ class Welcome extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
+	public function messages()	{
+		$data = array(
+      'title' => 'Aviation Support',
+  		'aircrafts' => $this->queries->get_aircrafts(),
+			'flights' => $this->queries->get_flights(),
+  		'engineTypes' => $this->queries->get_engine_types(),
+  		'aircraft_models' => $this->queries->get_aircraft_models(),
+  		'manufacturers' => $this->queries->get_manufacturers(),
+  		'locations' => $this->queries->get_locations(),
+      'schedule_types' => $this->queries->get_schedule_types(),
+      'inspection_types' => $this->queries->get_inspection_types(),
+      'task_categories' => $this->queries->get_task_categories(),
+      'schedule_categories' => $this->queries->get_schedule_categories(),
+      'ata_chapters' => $this->queries->get_ata_chapters(),
+      'comp_cats' => $this->queries->get_comp_cats(),
+			'trends' => $this->queries->get_trends()
+    );
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('messages', $data);
+		// modal view calls
+		$this->load->view('modals/add_aircraft');
+		$this->load->view('modals/add_flight');
+		$this->load->view('modals/add_task');
+
+		$this->load->view('templates/footer');
+	}
+
 	public function fleet(){
 		$data = array(
       'title' => 'Fleet',
