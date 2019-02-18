8a276dad590ac01f9f0efcede1ad757936be9a8b
megabreakage <martin.njau@live.com>
2019-02-18 11:39:50 +0300

    Creates new Authentication controller with login and logout methods

---
+++ application/controllers/Auth.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Martin Njuguna
+ * @version 1.0
+ * @see http://iosync.co.ke/
+ */
+class Auth extends CI_controller {
+
+
+public function index() {
+  $this->load->view('login');
+}
+
+public function login_user(){
+  $login_data = array(
+    'username' => $_POST['username'],
+    'password' => md5($_POST['password'])
+  );
+
+  $check = $this->queries->login_user($login_data);
+  if($check == FALSE){
+    echo json_encode(0);
+  } else {
+    $user_data = array(
+      'fname' => $check['fname'],
+      'lname' => $check['lname'],
+      'username' => $check['username'],
+      'role' => $check['user_role_id'],
+      'email' => $check['email'],
+      'loggedin' => TRUE
+    );
+    $this->session->set_userdata($user_data);
+    echo json_encode($user_data);
+  }
+}
+
+public function reset(){
+  echo "Enter your email address to receive a password reset";
+}
+
+public function logout(){
+  $this->session->sess_destroy();
+  redirect('', 'refresh');
+}
+
+
+} ?>
