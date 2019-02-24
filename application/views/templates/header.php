8fa37508f17e2dd2cc1be9caafac976b5f417236
megabreakage <martin.njau@live.com>
2019-02-24 08:19:27 +0300

    Adds a full project path

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,7 +20,7 @@
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <a class="navbar-brand" href="<?php echo base_url(); ?>">
-      <img src="images/logo3.png" alt="">
+      <img src="<?php echo base_url(); ?>images/logo3.png" alt="">
     </a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
